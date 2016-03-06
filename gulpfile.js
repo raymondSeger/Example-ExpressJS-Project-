f7910859cc9ecfa54fc53fd1934091909c092830
Raymond Seger <raymondseger@live.com>
2016-03-06 17:18:42 +0700

    added sourcemaps for the SASS and JS outputs

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
-var gulp 	= require('gulp');
-var minify 	= require('gulp-minify');
-var concat 	= require('gulp-concat');
-var uglify 	= require('gulp-uglify');
-var sass 	= require('gulp-sass');
+var gulp 		= require('gulp');
+var minify 		= require('gulp-minify');
+var concat 		= require('gulp-concat');
+var uglify 		= require('gulp-uglify');
+var sass 		= require('gulp-sass');
+var sourcemaps 	= require('gulp-sourcemaps');
 
 // concat then minify it https://www.npmjs.com/package/gulp-minify
 gulp.task('concatThenUglifyThenMinify', function() {
   gulp.src('resources/js/*.js')
+  	.pipe(sourcemaps.init())
     .pipe(concat('all.js'))
     .pipe(uglify())
     .pipe(minify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('public/dist/js'))
 });
 
 gulp.task('processSass', function () {
 
 	return gulp.src('./resources/sass/*.scss')
+		.pipe(sourcemaps.init())
 	    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	    .pipe(sourcemaps.write())
 	    .pipe(gulp.dest('./public/dist/css'));
-		
+
 });
 
 gulp.task('sass:watch', function () {
