cfa6a800a802ef8f652d0bc7ae2e3d7d9a59858a
Raymond Seger <raymondseger@live.com>
2016-03-06 17:12:20 +0700

    add gulp-sass to compile SASS files. I also added watch task to automatically run gulp command when there is change
    add gulp-sass to compile SASS files. I also added watch task to
    automatically run gulp command when there is change

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp 	= require('gulp');
 var minify 	= require('gulp-minify');
 var concat 	= require('gulp-concat');
 var uglify 	= require('gulp-uglify');
+var sass 	= require('gulp-sass');
 
 // concat then minify it https://www.npmjs.com/package/gulp-minify
 gulp.task('concatThenUglifyThenMinify', function() {
@@ -9,10 +10,19 @@ gulp.task('concatThenUglifyThenMinify', function() {
     .pipe(concat('all.js'))
     .pipe(uglify())
     .pipe(minify())
-    .pipe(gulp.dest('public/dist'))
+    .pipe(gulp.dest('public/dist/js'))
 });
 
+gulp.task('processSass', function () {
+  return gulp.src('./resources/sass/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/dist/css'));
+});
 
-gulp.task('default', ["concatThenUglifyThenMinify"], function() {
+gulp.task('sass:watch', function () {
+  gulp.watch('./resources/sass/*.scss', ['processSass']);
+});
+
+gulp.task('default', ["concatThenUglifyThenMinify", "processSass"], function() {
   // place code for your default task here
 });
