17b08b41b8a658512990d3fead532b9870993f3b
Raymond Seger <raymondseger@live.com>
2016-03-06 17:05:15 +0700

    added uglify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp 	= require('gulp');
 var minify 	= require('gulp-minify');
 var concat 	= require('gulp-concat');
+var uglify 	= require('gulp-uglify');
 
 // concat then minify it https://www.npmjs.com/package/gulp-minify
-gulp.task('concatThenMinify', function() {
+gulp.task('concatThenUglifyThenMinify', function() {
   gulp.src('resources/js/*.js')
     .pipe(concat('all.js'))
+    .pipe(uglify())
     .pipe(minify())
     .pipe(gulp.dest('public/dist'))
 });
 
 
-gulp.task('default', ["concatThenMinify"], function() {
+gulp.task('default', ["concatThenUglifyThenMinify"], function() {
   // place code for your default task here
 });
---
+++ public/dist/all.js
@@ -0,0 +1,2 @@
+var billy="my name is Billy",age="My age is 25";
+var JXXX="my name is JXXX",age2="My age is 23";
